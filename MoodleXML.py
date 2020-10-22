7c9b32191aaa1346a960f70561c1e0161428d8e1
Sven <sven.giese88@gmail.com>
2020-10-22 13:50:58 +0200

    Add documentation and clean code a bit - extended readme - filtering within the function call - requirements

--- MoodleXML.py
+++ MoodleXML.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from lxml import etree
 import copy
 import pandas as pd
@@ -5,12 +7,12 @@ import numpy as np
 
 
 one_correct = None
-two_correct= None
-three_correct= None
-four_correct= None
+two_correct = None
+three_correct = None
+four_correct = None
 
 #import re
-#def remove_control_characters(html):
+# def remove_control_characters(html):
 #    def str_to_int(s, default, base=10):
 #        if int(s, base) < 0x10000:
 #            return unichr(int(s, base))
@@ -19,6 +21,7 @@ four_correct= None
 #    html = re.sub(ur"&#[xX]([0-9a-fA-F]+);?", lambda c: str_to_int(c.group(1), c.group(0), base=16), html)
 #    html = re.sub(ur"[\x00-\x08\x0b\x0e-\x1f\x7f]", "", html)
 #    return (html)
+
 
 def create_category(parent, name, course):
     """
@@ -99,7 +102,7 @@ def add_question(parent, num_correct, title, text, answers, verbose=False):
     global two_correct
     global three_correct
     global four_correct
-    
+
     # choose the right template depending on the # of right answers
     if num_correct == 1:
         template = copy.deepcopy(one_correct)
@@ -112,75 +115,84 @@ def add_question(parent, num_correct, title, text, answers, verbose=False):
     else:
         raise ValueError("num_correct must be between 1 and 4")
 
-
     # modify template with the question data
     template.xpath('/question/name/text')[0].text = title.decode("utf-8")
-    if verbose:    
-        print ("Question: ", title)
-        print ("Text:", text.decode('utf-8'))
-        
+    if verbose:
+        print("Question: ", title)
+        print("Text:", text.decode('utf-8'))
+
     template.xpath('/question/questiontext/text')[0].text = etree.CDATA(text.decode("utf-8"))
     for m in range(len(template.xpath('/question/answer/text'))):
-        template.xpath('/question/answer/text')[m].text = etree.CDATA("%s" %answers[m].replace("$", "$$"))
+        template.xpath(
+            '/question/answer/text')[m].text = etree.CDATA("%s" % answers[m].replace("$", "$$"))
 
     # add question to root of main file
     parent.append(template.getroot())
 
 
-
-
-def TableToXML(table, outname, course):
+def TableToXML(table, outname, course, verbose=0):
     """
-    
+
     """
-    #%%
+    # %%
     global one_correct
     global two_correct
     global three_correct
     global four_correct
-    
+
     # import questions
     # the question table is exported from google docs as a csv.
     # The column names must be the same as in "BAI_Zwischentestat_Fragen.csv"
     try:
-        print ("Trying to read as CSV.")
-        questions = pd.read_csv(table, encoding = 'utf8', sep=",")
+        questions = pd.read_csv(table, sep=",")
     except:
-        print ("Trying to read as XLS.")
-        questions = pd.read_excel(table, 0, encoding = 'utf8')
-        
+        questions = pd.read_excel(table, 0)
+
+    # filter table
+    if "Anwendung" in questions.columns:
+        questions = questions[questions["Anwendung"] == 1]
+
+    # get categories
     categories = list(set(questions["Themenblock"].tolist()))
- 
+
     questions["WAHR"] = [str(i) for i in questions["WAHR"]]
     questions["FALSCH"] = [str(i) for i in questions["FALSCH"]]
-    
+
     # clean questions dataframe
-    print(questions["WAHR"].isnull().values.any()) 
+    if verbose:
+        print(questions["WAHR"].isnull().values.any())
     questions.replace("nan", "0", inplace=True)
     questions["WAHR"].fillna("0", inplace=True)
-    print(questions["WAHR"].isnull().values.any()) 
+    if verbose:
+        print(questions["WAHR"].isnull().values.any())
     questions["FALSCH"].fillna("0", inplace=True)
-    questions["WAHR"] = questions["WAHR"].map(lambda x: list(map(int, x.replace(".", ",").split(","))))
-    questions["FALSCH"] = questions["FALSCH"].map(lambda x: list(map(int, x.replace(".", ",").split(","))))
-    
+    questions["WAHR"] = questions["WAHR"].map(
+        lambda x: list(map(int, x.replace(".", ",").split(","))))
+    questions["FALSCH"] = questions["FALSCH"].map(
+        lambda x: list(map(int, x.replace(".", ",").split(","))))
+
     # check for errors in data
     questions["datacheck"] = questions["WAHR"] + questions["FALSCH"]
     for i in questions.index:
+        # see error codes for cases
         if len(questions.loc[i, "datacheck"]) != len((set(questions.loc[i, "datacheck"]))):
             raise ValueError("duplication in WAHR/FALSCH columns at index {}".format(i))
+
         if not (set(questions.loc[i, "datacheck"]) == set([0, 1, 2, 3, 4])
                 or set(questions.loc[i, "datacheck"]) == set([1, 2, 3, 4])):
             raise ValueError("Not all answers in WAHR/FALSCH column at index {}".format(i))
+
         if questions.loc[i, "WAHR"] == [0]:
             raise ValueError("zero in WAHR column at index {}".format(i))
+
         if pd.isnull(questions.loc[i, "Themenblock"]):
             raise ValueError("Empty category at index {}".format(i))
-    
+
     # add/rename columns
     questions["num_correct"] = questions["WAHR"].map(len)
     questions.rename(columns={'Hauptfrage': 'text', 'Themenblock': 'category'}, inplace=True)
     questions['answers'] = np.empty((len(questions), 0)).tolist()
-    
+
     # create answer list with right answers first, then wrong answers
     for i in questions.index:
         remaining = [1, 2, 3, 4]
@@ -191,55 +203,59 @@ def TableToXML(table, outname, course):
         for k in remaining:
             questions.loc[i, "answers"].append(
                 questions.loc[i, ["Aussage1", "Aussage2", "Aussage3", "Aussage4"]][k-1])
-    
+
     # configure parser: activate cdata and remove blank text at import
     parser = etree.XMLParser(strip_cdata=False, remove_blank_text=True)
-    
+
     # import template
     tree = etree.parse('template.xml', parser=parser)
     root = tree.getroot()
-    
+
     # import question template for every possible distribution of points.
     # This is necessary because the points have to add up to 100 in the ISIS System
     one_correct = etree.parse('one_correct.xml', parser=parser)
     two_correct = etree.parse('two_correct.xml', parser=parser)
     three_correct = etree.parse('three_correct.xml', parser=parser)
     four_correct = etree.parse('four_correct.xml', parser=parser)
-    
+
     # append questions and categories to xml file
     for i_category in categories:
-        print ("Processing category: {}".format(i_category.encode("UTF-8")))
-        
+        print("Processing category: {}".format(i_category.encode("UTF-8")))
+
         create_category(root, i_category, course)
         for i in questions[questions["category"] == i_category].index:
-            print ("Processing Question:")
-            print (questions.loc[i, "num_correct"])
-            print (questions.loc[i, "text"])
-            print (questions.loc[i, "answers"])
+            if verbose:
+                print("Processing Question:")
+                print(questions.loc[i, "num_correct"])
+                print(questions.loc[i, "text"])
+                print(questions.loc[i, "answers"])
             add_question(root,
                          num_correct=questions.loc[i, "num_correct"],
                          title=questions.loc[i, "text"].replace("$", "$$").encode("UTF-8")[0:38],
                          text=questions.loc[i, "text"].replace("$", "$$").encode("UTF-8"),
                          answers=questions.loc[i, "answers"])
-            print ("Done adding question.")
-    
+
     # write the xml file
-    tree.write(outname, encoding="UTF-8", xml_declaration=True, pretty_print=True)    
-    #%%
+    tree.write(outname, encoding="UTF-8", xml_declaration=True, pretty_print=True)
+    # %%
 
 
-
-# =============================================================================
-# Run File
-# =============================================================================
-import sys
-import os
-#reload(sys)
-#sys.setdefaultencoding('utf-8')
-#sys.exit()
+# cannot recall the purpose of this snippet, I had some utf8 issues earlier where this snippet
+# helped. Might not be necessary with python >= 3.
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+# sys.exit()
 # give the name of the table:
-table = "AdvancedBA.xlsx"
-course= "AdvancedBioanalytics"
+
+# this is the input data in xlsx format (table format, see readme)
+table = "template_advanced.xlsx"
+# course name, relevant for the name in moodle where the questions are stored (I believe...)
+course = "AdvancedBioanalytics"
+# save the xml file with the same name as input but with xml extension
 outname = table.split(".")[0]+".xml"
-TableToXML(table, outname, course)
-#os.system('pause')
+
+print(f"Reading {table}...")
+print(f"Generating questions for {course}...")
+TableToXML(table, outname, course, verbose=0)
+print(f"Done! File saved to {outname}...")
+
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # xls2moodle
 Script to convert multiple choice questions from xls to moodle's xml format
+
+## input
+The script requires an two arguments:
+- course name
+- location of tabular excel file with questions
+
+## output
+- xml format, ready for import in moodle
+
+## question format
+
+Please refer to the template_advanced.xlsx to get an ideao on the file format.
+Mandatory columns:
+- Aussage1 - possible answer 1
+- Aussage2 - possible answer 2
+- Aussage3 - possible answer 3
+- Aussage4 - possible answer 4
+- WAHR - contains index of correct answer(s), 1-based
+- FALSCH - contains index of wrong answer(s), 1-based
+- Themenblock - contains a topic name (this should be the same for similar questions)
+- Hauptfrage - Question text
+
+Optional columns:
+- Anwendung - boolean, filter to include (1) or exclude (0) questions
+
+Further columns are ignored.
+
+## known issues
+- latex equation code supported (?)
+- reading of data with special encoding (utf-8) currently not possible with the latest pandas version
